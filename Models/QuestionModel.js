07f690788760aa88f50404d6cb3a8cb3d5e9bd6f
Sabih-Ul-Hassan <80682421+Sabih-Ul-Hassan@users.noreply.github.com>
2023-06-12 02:06:39 +0500

    Update QuestionModel.js

--- Models/QuestionModel.js
+++ Models/QuestionModel.js
@@ -5,7 +5,7 @@ var QuestionSchema = mongoose.Schema({
     description:String,
     code:String,
     tags:[String],
-    answers:[{username:String,email:String,answers:String}]
+    answers:[{username:String,email:String,description:String}]
     
 })
 
